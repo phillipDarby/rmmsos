041dc2a2cac4871d23fc01e3b9cddf5101691e13
Phillip Darby <darby.phillip@gmail.com>
2021-01-17 13:54:56 -0500

    made db migration

---
+++ Server/Migrations/Sqlite/20210117185251_update devices.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Remotely.Server.Migrations.Sqlite
+{
+    public partial class updatedevices : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RunningProcess",
+                columns: table => new
+                {
+                    ID = table.Column<string>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    DeviceID = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RunningProcess", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_RunningProcess_Devices_DeviceID",
+                        column: x => x.DeviceID,
+                        principalTable: "Devices",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RunningProcess_DeviceID",
+                table: "RunningProcess",
+                column: "DeviceID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RunningProcess");
+        }
+    }
+}
